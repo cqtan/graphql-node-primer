1c084b3f493c91dbc7d57f64f84ff8bd5ccb48c1
ctan <s0547139@htw-berlin.de>
2019-10-19 22:24:20 +0200

    Add scalar type defs and resolvers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log("hello...wait for it....w...");
+import { GraphQLServer } from "graphql-yoga";
+
+// Type defs (schema)
+const typeDefs = `
+  type Query {
+    id: ID!
+    name: String!
+    age: Int!
+    employed: Boolean!
+    gpa: Float
+  }
+`;
+
+// Resolvers (functions that can be performed on this schema)
+const resolvers = {
+  Query: {
+    id: () => {
+      return "asdf1234";
+    },
+    name: () => {
+      return "CouchCat";
+    },
+    age: () => {
+      return 42;
+    },
+    employed: () => {
+      return true;
+    },
+    gpa: () => {
+      return 3.21;
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("Server is running...");
+});
