fb7d6d8b1a56d4c190f9a92a665ec001c36a94a9
ctan <s0547139@htw-berlin.de>
2019-10-19 22:48:32 +0200

    Add gql variables

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import { GraphQLServer } from "graphql-yoga"
 // Type defs (schema)
 const typeDefs = `
   type Query {
+    greeting(name: String, position: String!): String!
     me: User!
+    post: Post!
   }
 
   type User {
@@ -12,16 +14,38 @@ const typeDefs = `
     email: String!
     age: Int
   }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean!
+  }
 `
 
 // Resolvers (functions that can be performed on this schema)
 const resolvers = {
   Query: {
+    greeting: (parent, args, ctx, info) => {
+      if (args.name && args.position) {
+        return `Hello, ${args.name} you are ${args.position}`
+      } else {
+        return "Hoi! "
+      }
+    },
     me: () => {
       return {
         id: "asdf1234",
         name: "CouchCat",
         email: "couch@cat.com"
+      }
+    },
+    post: () => {
+      return {
+        id: "123",
+        title: "Best title",
+        body: "This is the content of the best post",
+        published: true
       }
     }
   }
