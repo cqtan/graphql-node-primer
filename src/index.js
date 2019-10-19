aca800789f14449bb5c0d2b1e6c7fcf3c446d71a
ctan <s0547139@htw-berlin.de>
2019-10-19 22:36:15 +0200

    Add Custom User type

--- src/index.js
+++ src/index.js
@@ -1,42 +1,37 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer } from "graphql-yoga"
 
 // Type defs (schema)
 const typeDefs = `
   type Query {
+    me: User!
+  }
+
+  type User {
     id: ID!
     name: String!
-    age: Int!
-    employed: Boolean!
-    gpa: Float
+    email: String!
+    age: Int
   }
-`;
+`
 
 // Resolvers (functions that can be performed on this schema)
 const resolvers = {
   Query: {
-    id: () => {
-      return "asdf1234";
-    },
-    name: () => {
-      return "CouchCat";
-    },
-    age: () => {
-      return 42;
-    },
-    employed: () => {
-      return true;
-    },
-    gpa: () => {
-      return 3.21;
+    me: () => {
+      return {
+        id: "asdf1234",
+        name: "CouchCat",
+        email: "couch@cat.com"
+      }
     }
   }
-};
+}
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers
-});
+})
 
 server.start(() => {
-  console.log("Server is running...");
-});
+  console.log("Server is running...")
+})
