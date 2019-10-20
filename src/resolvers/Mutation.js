eb428df85bdfc79f1d1a7d2aeee2192d371adba2
ctan <s0547139@htw-berlin.de>
2019-10-20 17:25:38 +0200

    Add update mutation for each entity, done with crud

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -34,6 +34,30 @@ const Mutation = {
 
     return deletedUsers[0]
   },
+  updateUser: (parent, args, { db }, info) => {
+    const { id, data } = args
+    const user = db.users.find(user => user.id === id)
+
+    if (!user) throw new Error("User not found")
+
+    if (typeof data.email === "string") {
+      const emailTaken = db.users.some(user => user.email === data.email)
+
+      if (emailTaken) throw new Error("Email taken")
+
+      user.email = data.email
+    }
+
+    if (typeof data.name === "string") {
+      user.name = data.name
+    }
+
+    if (typeof data.age === "undefined") {
+      user.age = data.age
+    }
+
+    return user
+  },
   createPost: (parent, args, { db }, info) => {
     const userExists = db.users.some(user => user.id === args.data.author)
 
@@ -58,6 +82,26 @@ const Mutation = {
     db.comments = db.comments.filter(comment => comment.post !== args.id)
 
     return deletedPost[0]
+  },
+  updatePost: (parent, args, { db }, info) => {
+    const { id, data } = args
+    const post = db.posts.find(post => post.id === args.id)
+
+    if (!post) throw new Error("Post not found")
+
+    if (typeof data.title === "string") {
+      post.title = data.title
+    }
+
+    if (typeof data.body === "string") {
+      post.body = data.body
+    }
+
+    if (typeof data.published === "boolean") {
+      post.published = data.published
+    }
+
+    return post
   },
   createComment: (parent, args, { db }, info) => {
     const userExists = db.users.some(user => user.id === args.data.author)
@@ -85,6 +129,18 @@ const Mutation = {
     const deletedComment = db.comments.splice(commentIndex, 1)
 
     return deletedComment[0]
+  },
+  updateComment: (parent, args, { db }, info) => {
+    const { id, data } = args
+    const comment = db.comments.find(comment => comment.id === args.id)
+
+    if (!comment) throw new Error("No Comment found")
+
+    if (typeof data.text === "string") {
+      comment.text = data.text
+    }
+
+    return comment
   }
 }
 
