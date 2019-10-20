f50788180f184d0a9ff458a48794e1f5385b08b9
ctan <s0547139@htw-berlin.de>
2019-10-20 12:50:13 +0200

    Add babel spread plugin and refactor mutations

--- src/index.js
+++ src/index.js
@@ -166,9 +166,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       }
 
       users.push(user)
@@ -182,10 +180,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args
       }
 
       posts.push(post)
@@ -203,9 +198,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args
       }
 
       return comment
