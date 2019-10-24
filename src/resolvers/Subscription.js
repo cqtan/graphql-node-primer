d599fdbcaa172c053252d86ea259b57324be5eec
ctan <s0547139@htw-berlin.de>
2019-10-24 18:37:38 +0200

    Refactor Subscriptions with Prisma

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,30 +1,40 @@
 const Subscription = {
-  count: {
-    subscribe: (parent, args, { pubsub }, info) => {
-      let count = 0
-
-      setInterval(() => {
-        count++
-        pubsub.publish("count", {
-          count
-        })
-      }, 1000)
-
-      return pubsub.asyncIterator("count")
-    }
-  },
   comment: {
-    subscribe: (parent, { postId }, { db, pubsub }, info) => {
-      const post = db.posts.find(post => post.id === postId && post.published)
+    subscribe: (parent, { postId }, { prisma }, info) => {
+      return prisma.subscription.comment(
+        {
+          where: {
+            node: {
+              post: {
+                id: postId
+              }
+            }
+          }
+        },
+        info
+      )
 
-      if (!post) throw new Error("Post not found")
+      // const post = db.posts.find(post => post.id === postId && post.published)
 
-      return pubsub.asyncIterator(`comment ${postId}`)
+      // if (!post) throw new Error("Post not found")
+
+      // return pubsub.asyncIterator(`comment ${postId}`)
     }
   },
   post: {
-    subscribe: (parent, args, { db, pubsub }, info) => {
-      return pubsub.asyncIterator("post")
+    subscribe: (parent, args, { prisma }, info) => {
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              published: true
+            }
+          }
+        },
+        info
+      )
+
+      // return pubsub.asyncIterator("post")
     }
   }
 }
