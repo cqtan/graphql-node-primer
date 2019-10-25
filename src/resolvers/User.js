f36682547e2e2b65e059b04141aa03bd6a772a5b
ctan <s0547139@htw-berlin.de>
2019-10-26 00:00:41 +0200

    Add Fragments

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -13,13 +13,24 @@ const User = {
   //     return comment.author === parent.id
   //   })
   // }
-  email: (parent, args, { request }, info) => {
-    const userId = getUserId(request, false)
+  posts: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { prisma }, info) {
+      return prisma.query.posts({
+        where: { published: true, author: { id: parent.id } }
+      })
+    }
+  },
+  email: {
+    fragment: "fragment userId on User { id }",
+    resolve(parent, args, { request }, info) {
+      const userId = getUserId(request, false)
 
-    if (userId && userId === parent.id) {
-      return parent.email
-    } else {
-      return null
+      if (userId && userId === parent.id) {
+        return parent.email
+      } else {
+        return null
+      }
     }
   }
 }
