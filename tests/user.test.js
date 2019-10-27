b0774903dd8205fd3c908c25fc8b3f61cee1f199
ctan <s0547139@htw-berlin.de>
2019-10-27 16:01:28 +0100

    Add first complete test

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,9 +2,48 @@ import "@babel/polyfill/noConflict"
 import "cross-fetch/polyfill"
 import ApolloBoost, { gql } from "apollo-boost"
 import prisma from "../src/prisma"
+import bcrypt from "bcryptjs"
 
 const client = new ApolloBoost({
   uri: "http://localhost:4000"
+})
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts()
+  await prisma.mutation.deleteManyUsers()
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: "Dog",
+      email: "dog@test.com",
+      password: bcrypt.hashSync("asdf1234")
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: "TestPost1",
+      body: "TestPost1 Body",
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: "TestPost2",
+      body: "TestPost2 Body",
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
 })
 
 test("Should create a new user", async () => {
